4770abdfe1dc14703ad17b133b1b7e10c7a9b7a4
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-05-06 13:51:44 -0700

    Added CSV/TSV support.

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.apache.jena/jena-core "2.10.0"]
                  [org.apache.jena/jena-arq "2.10.0"]
+                 [org.clojure/data.csv "0.1.2"]
                  [com.cemerick/url "0.0.7"]]
   :profiles {:dev {:dependencies [[midje "1.4.0"]
                                   [lein-midje "2.0.0-SNAPSHOT"]]}}
---
+++ src/hoot/csv.clj
@@ -0,0 +1,21 @@
+(ns hoot.csv
+  (:require [clojure.string :as string]))
+
+(defn read-csv
+  [instream & {:keys [separator] :or {separator #","}}]
+  (mapv #(string/split %1 separator) (string/split-lines (slurp instream))))
+
+(defn read-tsv [instream] (read-csv instream :separator #"\t"))
+
+(defn extract-triples
+  [row]
+  ())
+
+(defn rows->triples
+  [rows]
+  (mapv #(hash-map :subject (nth %1 0 "") :predicate (nth %1 1 "") :object (nth %1 2 "")) rows))
+
+(defn statements
+  [instream type]
+  (let [parser (if (= (string/lower-case type) "tsv") read-tsv read-csv)]
+    (rows->triples (parser instream))))
